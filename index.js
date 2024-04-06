6b280a86cd3f148d8dad05fa03f6fcb0e58c1354
Linet <teweilinet@gmail.com>
2024-04-06 13:32:36 +0300

    Using Fetch

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-function fetchBooks() {
+
   // To pass the tests, don't forget to return your fetch!
+  function fetchBooks() {
   
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then(data => {renderBooks(data); console.log(data)});
+  
+  }
+  
+  function renderBooks(books) {
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchBooks();
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
